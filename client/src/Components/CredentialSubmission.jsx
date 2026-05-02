7df37079bb6c8a7e15c6cbd9524f6abbd8038076
sagrika thakur <sagrikathakur68@gmail.com>
2026-05-02 23:05:00 +0530

    started working on credentialsumbmission.jsx

---
+++ client/src/Components/CredentialSubmission.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CredentialSubmission = () => {
+
+  const
+
+
+
+
+
+
+  return (
+    <div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default CredentialSubmission
