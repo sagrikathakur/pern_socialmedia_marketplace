4b3ebd442b6401eae5189e81f4368c3051b0c024
sagrika thakur <sagrikathakur68@gmail.com>
2026-06-17 17:37:34 +0530

    facing problem while integration with inngest

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import "dotenv/config";
 import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT || 8000}`);
