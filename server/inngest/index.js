b4446adf3287c37ca3124dd7bf362518012e8f07
sagrika thakur <sagrikathakur68@gmail.com>
2026-06-20 02:06:42 +0530

    adding vercel.json

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -16,17 +16,97 @@ const syncUserCreation = inngest.createFunction(
         where: { id: data.id },
         data: {
           email: data?.email_addresses[0].email_address,
-          name: data.username || `${data.first_name} ${data.last_name}`,
-          image: data.image_url,
+          name: data?.first_name + " " + data?.last_name,
+          image: data?.image_url,
         }
       })
+      return;
     }
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0].email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      }
+    })
 
 
   }
 );
 
 
+// to delete//
+
+
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { data } = event
+    const listings = await prisma.listing.findMany({
+      where: { ownerId: data.id }
+    })
+    const chats = await prisma.chat.findMany({
+      where: { OR: [{ ownerUserId: data.id }, { chatUserId: data.id }] }
+    })
+    const transactions = await prisma.transaction.findMany({
+      where: { userId: data.id }
+    })
+    if (
+      listings.length === 0 && chats.length === 0 && transactions.length === 0) {
+      await prisma.user.delete({
+        where: { id: data.id }
+      })
+
+    } else {
+      await prisma.listing.updateMany({
+        where: { ownerId: data.id },
+        data: { status: 'inactive' }
+      })
+    }
+
+
+  }
+
+
+
+);
+
+// update/ /
+const syncUserUpdate = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { data } = event
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        email: data?.email_addresses[0].email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      }
+    })
+  })
+if (!user) {
+  await prisma.user.create({
+    data: {
+      id: data.id,
+      email: data?.email_addresses[0].email_address,
+      name: data?.first_name + " " + data?.last_name,
+      image: data?.image_url,
+    }
+  })
+  return;
+}
+await prisma.user.update({
+  where: { id: data.id },
+  data: {
+    email: data?.email_addresses[0].email_address,
+    name: data?.first_name + " " + data?.last_name,
+    image: data?.image_url,
+  }
+})
 
 
 
@@ -37,6 +117,9 @@ const syncUserCreation = inngest.createFunction(
 
 
 
-
-export const functions = []
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdate
+]
 
